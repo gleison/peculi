ef628e79aadae9605bffeda69b662217c9a0b164
= <=>
2023-10-29 15:30:07 -0300

    Go: basic web app with html return

--- cmd/peculi/main.go
+++ cmd/peculi/main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
 	"github.com/gleison/peculi/internal/model"
 	"github.com/gleison/peculi/internal/service"
 )
@@ -8,4 +13,14 @@ import (
 func main() {
 	service.SayHello()
 	model.SayHi()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", index)
+
+	err := http.ListenAndServe(":3333", mux)
+	fmt.Printf("Error: %v\n", err)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	index, _ := os.ReadFile("web/index.html")
+	io.WriteString(w, string(index))
 }
